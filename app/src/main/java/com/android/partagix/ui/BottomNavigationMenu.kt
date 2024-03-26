48b583de2dea9c0759062688d9fc4039cd1c28f6
Valentin Schneeberger <valentin2.schneeberger@gmail.com>
2024-03-26 17:05:16 +0100

    Add default navigation bottom bar

---
+++ app/src/main/java/com/android/partagix/ui/BottomNavigationMenu.kt
@@ -0,0 +1,130 @@
+package com.android.partagix.ui
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.android.partagix.ui.navigation.NavigationActions
+import com.android.partagix.ui.navigation.TOP_LEVEL_DESTINATIONS
+import com.android.partagix.ui.navigation.TopLevelDestination
+
+
+@Composable
+fun BottomNavigationMenu(
+    onTabSelect: (String) -> Unit,
+    tabList: List<TopLevelDestination>,
+    selectedItem: String
+) {
+
+}
+
+
+
+/*
+
+@Composable
+fun MyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    println("----- We are inside nav host")
+
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Route.INBOX,
+    ) {
+        composable(Route.INBOX) {
+            val navigate = NavigationActions::navigateTo
+            ScaffoldExample(
+                selectedDestination = "Inbox",
+                navigateToTopLevelDestination = { navigate }
+            )
+        }
+        composable(Route.DM) {
+            EmptyComingSoon()
+        }
+        composable(Route.ARTICLES) {
+            EmptyComingSoon()
+        }
+        composable(Route.GROUPS) {
+            EmptyComingSoon()
+        }
+    }
+}
+ */
+
+@Composable
+fun BottomNavigationBar(
+    selectedDestination: String,
+    navigateToTopLevelDestination: (TopLevelDestination) -> Unit
+) {
+    NavigationBar(modifier = Modifier.fillMaxWidth()) {
+        TOP_LEVEL_DESTINATIONS.forEach { destination ->
+
+            NavigationBarItem(
+                selected = selectedDestination == destination.route,
+                onClick = {
+                    println("----- This is a click on " + destination.route)
+                    navigateToTopLevelDestination(destination)
+                },
+                icon = {
+                    Icon(
+                        imageVector = destination.icon,
+                        contentDescription = null
+                    )
+                }
+
+            )
+
+        }
+            /*
+                NavigationBarItem(
+                    selected = selectedDestination == destination.route,
+                    onClick = {
+                        //print("This is a click")
+                        //navigateToTopLevelDestination(destination)
+                    },
+                    icon = {
+                        androidx.compose.material3.Icon(
+                            imageVector = destination.icon,
+                            contentDescription = stringResource(id = destination.textId)
+                        )
+                    }
+                )
+             */
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun previewScaffold() {
+    val navController = rememberNavController()
+    val navigate = NavigationActions::navigateTo
+
+}
