d455731c3e3583ae6b67bb4981cca6c174fab6cf
Patrick Pataky <patrick.pataky@epfl.ch>
2024-03-27 20:05:20 +0100

    Fix for not building correctly with Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -157,6 +157,9 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
+        {
+            exclude(group = "com.google.protobuf", module="protobuf-java")
+        }
 
     implementation("com.google.android.gms:play-services-auth:20.6.0")
 
