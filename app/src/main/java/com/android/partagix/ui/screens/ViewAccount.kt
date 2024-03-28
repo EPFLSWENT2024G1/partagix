78e25465976299f73263defcf784363d017452ef
Antoine Buttier <antoine.buttier@epfl.ch>
2024-03-28 20:25:35 +0100

    First functioning Account screen

--- app/src/main/java/com/android/partagix/ui/screens/ViewAccount.kt
+++ app/src/main/java/com/android/partagix/ui/screens/ViewAccount.kt
@@ -1,6 +1,7 @@
 package com.android.partagix.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircle
+
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,14 +28,18 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.android.partagix.R
+import kotlin.math.round
 
 @Preview(showBackground = true)
 @OptIn(ExperimentalMaterial3Api::class)
@@ -43,12 +50,23 @@ fun ViewAccount() {
             TopAppBar(
                 title = { Text("My profile" /*TODO: get username to display custom message*/) },
                 modifier = Modifier.fillMaxWidth(),
+                navigationIcon = {
+                    IconButton(onClick = {/*TODO: navigate to previous screen*/ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            modifier = Modifier.width(48.dp)
+                        )
+                    }
+                }
+
                 )
         },
-        modifier = Modifier.fillMaxWidth()){
+        modifier = Modifier.fillMaxSize())
+    {
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxHeight()
                 .padding(it)
                 .verticalScroll(rememberScrollState())
         ) {
@@ -57,62 +75,105 @@ fun ViewAccount() {
                 painterResource(
                     id = R.drawable.ic_launcher_background) /*TODO: get profile picture*/,
                 contentDescription = null,
-                alignment = Alignment.BottomCenter)
+                modifier = Modifier
+                    .fillMaxWidth(),
+                alignment = Alignment.Center)
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.Absolute.SpaceAround
             ) {
-                Text("username" /*TODO: get username*/)
-                Text("'s profile")
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
-                Text("location" /*TODO: get user location / NPA*/)
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Text("Trust ")
-                var rating = 4.82 /*TODO: get user rating*/
-                val roundedRating = (2*rating).toInt()
-                when (roundedRating) {
-                    0 -> {
-                        Text("☆☆☆☆☆")
-                    }
-                    1 -> {
-                        Text("☆☆☆☆")
-                    }
-                    2 -> {
-                        Text("★☆☆☆☆")
-                    }
-                    4 -> {
-                        Text("★★☆☆☆")
-                    }
-                    6 -> {
-                        Text("★★★☆☆")
-                    }
-                    8 -> {
-                        Text("★★★★☆")
-                    }
-                    10 -> {
-                        Text("★★★★★")
-                    }
-                }
+                val username = "antoine" // TODO: get username
+                Text("$username's profile")
             }
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = { /*TODO: navigate to inventory */ }) {
-            Text("See inventory")
+            TextField(
+                value = "Chavannes", /*TODO: get item disponibility*/
+                onValueChange = {},
+                label = { Text("Location") },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.Gray,
+                    unfocusedIndicatorColor = Color.Gray,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent
+
+                    ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                readOnly = true,
+
+                leadingIcon = {Icon(Icons.Default.LocationOn, contentDescription = null)}
+            )
+            var rating = round(4.42345678 * 100) / 100  /*TODO: get user rating*/
+            val roundedRating = round(rating).toInt()
+            val stars = when (roundedRating) {
+                0 -> {
+                    "☆☆☆☆☆ ($rating/5)"
+                }
+                1 -> {
+                    "★☆☆☆☆ ($rating/5)"
+                }
+                2 -> {
+                    "★★☆☆☆ ($rating/5)"
+                }
+                3 -> {
+                    "★★★☆☆ ($rating/5)"
+                }
+                4 -> {
+                    "★★★★☆ ($rating/5)"
+                }
+                5 -> {
+                    "★★★★★ ($rating/5)"
+                }
+                else -> {
+                    "c'est tout cassé mon reuf"
+                }
             }
+            TextField(
+
+
+                value = stars,
+                onValueChange = {},
+                label = { Text("Trust") },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Gray,
+                    disabledIndicatorColor = Color.Gray,
+                    unfocusedIndicatorColor = Color.Gray,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent
+
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                readOnly = true,
+                leadingIcon = {Icon(Icons.Default.CheckCircle, contentDescription = null)}
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { /*TODO: friends */ }) {
-                Text("Add as friend")
+            Row (modifier = Modifier.fillMaxWidth().padding(8.dp, 0.dp)){
+                Button(onClick = { /*TODO: navigate to inventory */ },
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                    ) {
+                    Text("See inventory")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = { /*TODO: friends */ },
+                    modifier = Modifier.fillMaxWidth()
+
+                ) {
+                    Text("Add as friend")
+
+                }
+
+
             }
+
+
+
         }
 
     }
