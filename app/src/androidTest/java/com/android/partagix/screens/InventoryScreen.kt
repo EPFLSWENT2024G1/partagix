ecfb799606962e4824f74c70918fdbb24539a12d
KassFlute <cassien.roth@epfl.ch>
2024-03-28 18:06:24 +0100

    create InventoryScreen.kt to test the inventory screen

---
+++ app/src/androidTest/java/com/android/partagix/screens/InventoryScreen.kt
@@ -0,0 +1,12 @@
+package com.android.partagix.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+
+class InventoryScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<InventoryScreen>(
+        semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("createScreen") }) {
+
+    val screenTitle: KNode = onNode { hasTestTag("inventoryScreen") }
+}
