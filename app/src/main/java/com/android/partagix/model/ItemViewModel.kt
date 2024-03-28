5106005aeaedd200db5fd00b58a72e2c20574aee
Massimo Berardi <massimo.berardi@epfl.ch>
2024-03-28 18:13:14 +0100

    ItemViewModel.kt :     Created and adapted the file based on InventoryViewModel.kt     Upgrade : the call "database.getItems{ update(it[0])" inside getItem() could be cleaner with a dedicated database function returning only one Item and not a List<Item>

---
+++ app/src/main/java/com/android/partagix/model/ItemViewModel.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.partagix.model
+
+import Item
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class ItemViewModel(item : Item) : ViewModel() {
+
+    private val database = Database()
+
+    // UI state exposed to the UI
+    private val _uiState =
+        MutableStateFlow(
+            ItemUIState(
+                item
+            ))
+    val uiState: StateFlow<ItemUIState> = _uiState
+
+    init {
+        getItem()
+    }
+
+    private fun getItem() {
+        viewModelScope.launch { database.getItems{ update(it[0]) } }
+    /* weird notation "it[0]" since the getter returns a list of Items
+    and we want the unique Item that's contained in it
+    -> upgrade idea : create a getter also for Item */
+    }
+
+    private fun update(new: Item) {
+
+        _uiState.value =
+            _uiState.value.copy(
+                item = new
+            )
+    }
+}
+
+data class ItemUIState(val item : Item)
