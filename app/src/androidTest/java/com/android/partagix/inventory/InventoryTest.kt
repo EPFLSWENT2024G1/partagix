6ac1513e9466ceec0dcbc5719746342c86428a1f
KassFlute <cassien.roth@epfl.ch>
2024-03-29 14:19:27 +0100

    Implement working test structure for the InventoryScreen

--- app/src/androidTest/java/com/android/partagix/inventory/InventoryTest.kt
+++ app/src/androidTest/java/com/android/partagix/inventory/InventoryTest.kt
@@ -1,15 +1,25 @@
 package com.android.partagix.inventory
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.partagix.model.InventoryUIState
 import com.android.partagix.model.InventoryViewModel
 import com.android.partagix.screens.InventoryScreen
 import com.android.partagix.ui.navigation.NavigationActions
+import com.android.partagix.ui.navigation.Route
 import com.android.partagix.ui.screens.InventoryScreen
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.just
+import io.mockk.mockk
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -19,15 +29,21 @@ import org.junit.runner.RunWith
 class InventoryTest: TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
     @get:Rule val composeTestRule = createComposeRule()
+    @RelaxedMockK lateinit var mockNavActions: NavigationActions
+    @RelaxedMockK lateinit var mockInventoryViewModel: InventoryViewModel
 
-    //@get:Rule val mockkRule = MockKRule(this)
-
-    lateinit var navActions: NavigationActions
+    private val mockUiState = MutableStateFlow(InventoryUIState(emptyList()))
 
     @Before
     fun testSetup() {
-        val vm = InventoryViewModel()
-        composeTestRule.setContent { InventoryScreen(vm, navActions::navigateTo) }
+        mockInventoryViewModel = mockk()
+        every { mockInventoryViewModel.uiState } returns mockUiState
+
+        mockNavActions = mockk<NavigationActions>()
+        every { mockNavActions.navigateTo(Route.HOME) } just Runs
+        every { mockNavActions.navigateTo(Route.LOGIN) } just Runs
+
+        composeTestRule.setContent { InventoryScreen(mockInventoryViewModel, mockNavActions) }
     }
 
     @Test
@@ -35,7 +51,7 @@ class InventoryTest: TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSu
         onComposeScreen<InventoryScreen>(composeTestRule) {
             mainContentText {
                 assertIsDisplayed()
-                assertTextContains("There is")
+                assertTextContains(value="There is", substring=true, ignoreCase=true)
             }
         }
     }
