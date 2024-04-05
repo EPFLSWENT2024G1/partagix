ced5a48e4a24981ea63ddc0fc6cf0803300f2fe7
Patrick Pataky <patrick.pataky@epfl.ch>
2024-04-05 11:50:09 +0200

    Added button for inventory screen from home screen

--- app/src/main/java/com/android/partagix/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/android/partagix/ui/screens/HomeScreen.kt
@@ -1,9 +1,16 @@
 package com.android.partagix.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.android.partagix.ui.navigation.NavigationActions
+import com.android.partagix.ui.navigation.Route
 
 @Composable
-fun HomeScreen() {
-  Text("Home Screen")
+fun HomeScreen(navigationActions: NavigationActions) {
+  Column {
+    Text("Home Screen")
+    Button(onClick = { navigationActions.navigateTo(Route.INVENTORY) }) { Text("Go to Inventory") }
+  }
 }
