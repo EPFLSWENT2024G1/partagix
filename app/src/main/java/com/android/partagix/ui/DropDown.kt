baf804e96b958a903ca97c40484ea9f250245493
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-04-09 11:27:03 +0200

    Fixed the drop down so we can get the value selected

--- app/src/main/java/com/android/partagix/ui/DropDown.kt
+++ app/src/main/java/com/android/partagix/ui/DropDown.kt
@@ -23,14 +23,14 @@ import androidx.compose.ui.unit.dp
  * @param list the list of items to show in the dropdown, ex : CategoryItems, VisibilityItems...
  */
 @Composable
-fun DropDown(label: String, list: List<String>) {
+fun DropDown(label: String, list: List<String>): String {
+  val expend = remember { mutableStateOf(false) }
+  val selectedValue = remember { mutableStateOf(label) }
   Box(modifier = Modifier.fillMaxWidth()) {
-    val expend = remember { mutableStateOf(false) }
-    val selectedCategory = remember { mutableStateOf(label) }
     Column(modifier = Modifier.fillMaxWidth()) {
       OutlinedButton(
           onClick = { expend.value = true },
-          content = { Text(selectedCategory.value) },
+          content = { Text(selectedValue.value) },
           modifier = Modifier.fillMaxWidth(),
           shape = RoundedCornerShape(10))
 
@@ -44,7 +44,7 @@ fun DropDown(label: String, list: List<String>) {
           list.forEach {
             DropdownMenuItem(
                 onClick = {
-                  selectedCategory.value = it
+                  selectedValue.value = it
                   expend.value = false
                 },
                 text = { Text(it) })
@@ -53,6 +53,7 @@ fun DropDown(label: String, list: List<String>) {
       }
     }
   }
+  return selectedValue.value
 }
 
 val CategoryItems =
