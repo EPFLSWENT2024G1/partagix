51a1990c37c6296e6bf76c2b95ba34c0703d931b
Massimo Berardi <massimo.berardi@epfl.ch>
2024-04-10 17:40:09 +0200

    Created corresponding enum and viewmodel

---
+++ app/src/main/java/com/android/partagix/model/StampViewModel.kt
@@ -0,0 +1,1 @@
+package com.android.partagix.model
---
+++ app/src/main/java/com/android/partagix/model/stampDimension/StampDimension.kt
@@ -0,0 +1,8 @@
+package com.android.partagix.model.stampDimension
+
+enum class StampDimension {
+  SMALL, // ordinal = 0
+  MEDIUM, // ordinal = 1
+  BIG, // ordinal = 2
+  FULL_PAGE // ordinal = 3
+}
