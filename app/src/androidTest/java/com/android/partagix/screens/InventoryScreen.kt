8e5e8927924a7b6b8cdf2f2ea23095891305fdc3
KassFlute <cassien.roth@epfl.ch>
2024-04-11 01:21:30 +0200

    Update inventory test screen for new test tags

--- app/src/androidTest/java/com/android/partagix/screens/InventoryScreen.kt
+++ app/src/androidTest/java/com/android/partagix/screens/InventoryScreen.kt
@@ -10,10 +10,17 @@ class InventoryScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
         viewBuilderAction = { hasTestTag("inventoryScreen") }) {
 
   // val screenTitle: KNode = onNode { hasTestTag("inventoryScreen") }
-  val mainContent: KNode = onNode { hasTestTag("inventoryScreenMainContent") }
-  val mainContentText: KNode = onNode { hasTestTag("inventoryScreenMainContentText") }
-  val bottomNavBar: KNode = onNode { hasTestTag("inventoryScreenBottomNavBar") }
+  val searchBar: KNode = onNode { hasTestTag("inventoryScreenSearchBar") }
+    val searchBarBackIcon: KNode = searchBar.child { hasTestTag("inventoryScreenSearchBarBack") }
+    val searchBarSearchIcon: KNode = searchBar.child { hasTestTag("inventoryScreenSearchBarSearch") }
+  // val mainContentText: KNode = onNode { hasTestTag("inventoryScreenMainContentText") }
 
+  val fab: KNode = onNode { hasTestTag("inventoryScreenFab") }
+  val noItemBox: KNode = onNode { hasTestTag("inventoryScreenNoItemBox") }
+  val noItemText: KNode = onNode { hasTestTag("inventoryScreenNoItemText") }
+  val itemList: KNode = onNode { hasTestTag("inventoryScreenItemList") }
+
+  val bottomNavBar: KNode = onNode { hasTestTag("inventoryScreenBottomNavBar") }
   val bottomNavBarItemInventory: KNode =
       bottomNavBar.child { hasTestTag("bottomNavBarItem-Inventory") }
 }
