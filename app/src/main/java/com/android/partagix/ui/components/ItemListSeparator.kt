f4af1d6fb96566feedf4607cdbdab1c5d669e377
skyrag3 <nicolas.duprat@epfl.ch>
2024-04-11 15:04:46 +0200

    making sure the corner text is actually in the corner

--- app/src/main/java/com/android/partagix/ui/components/ItemListSeparator.kt
+++ app/src/main/java/com/android/partagix/ui/components/ItemListSeparator.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.android.partagix.model.item.Item
 
@@ -24,11 +27,12 @@ fun ItemListColumn(List: List<Item>, Title : String, corner : String,onClick:(It
                 fontSize = 18.sp,
                 fontWeight = FontWeight(500),
                 color = Color(0xFF000000),
-
                 ),
+                modifier = Modifier.width(120.dp)
                 )
 
             Text(text = corner,
+                textAlign = TextAlign.Right,
                 modifier = Modifier.clickable{onClickCorner()}
             )
 
