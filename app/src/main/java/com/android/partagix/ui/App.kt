9277b443de689546fc4d08709a409519647bdc4e
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-04-12 13:44:31 +0200

    should work

--- app/src/main/java/com/android/partagix/ui/App.kt
+++ app/src/main/java/com/android/partagix/ui/App.kt
@@ -115,14 +115,11 @@ class App(activity: MainActivity) : ComponentActivity(), SignInResultListener {
     ) {
       composable(Route.BOOT) { BootScreen(authentication, navigationActions, modifier) }
       composable(Route.LOGIN) { LoginScreen(authentication, modifier) }
-      composable(Route.HOME) {
-          println("home")
-          HomeScreen(navigationActions) }
+      composable(Route.HOME) { HomeScreen(navigationActions) }
       composable(Route.BORROW) {
         HomeScreen(navigationActions) /*TODO:Change to the borrow screen*/
       }
       composable(Route.INVENTORY) {
-          println("inventory")
         InventoryScreen(
             inventoryViewModel = inventoryViewModel,
             navigateToTopLevelDestination = navigationActions::navigateTo)
