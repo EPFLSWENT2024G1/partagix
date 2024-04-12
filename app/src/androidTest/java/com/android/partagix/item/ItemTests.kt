7afad81bfad1e85c5461e64f99574227ab5112c8
KassFlute <cassien.roth@epfl.ch>
2024-04-12 14:01:54 +0200

    Format and adapt to new Item fields

--- app/src/androidTest/java/com/android/partagix/item/ItemTests.kt
+++ app/src/androidTest/java/com/android/partagix/item/ItemTests.kt
@@ -2,8 +2,6 @@ package com.android.partagix.item
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.android.partagix.model.InventoryUIState
-import com.android.partagix.model.InventoryViewModel
 import com.android.partagix.model.ItemUIState
 import com.android.partagix.model.ItemViewModel
 import com.android.partagix.model.category.Category
@@ -12,7 +10,6 @@ import com.android.partagix.model.visibility.Visibility
 import com.android.partagix.screens.InventoryViewItem
 import com.android.partagix.ui.navigation.NavigationActions
 import com.android.partagix.ui.navigation.Route
-import com.android.partagix.ui.screens.InventoryScreen
 import com.android.partagix.ui.screens.InventoryViewItem
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
@@ -32,76 +29,64 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ItemTests : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
-    @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
-    @RelaxedMockK lateinit var mockNavActions: NavigationActions
-    @RelaxedMockK lateinit var mockItemViewModel: ItemViewModel
+  @RelaxedMockK lateinit var mockNavActions: NavigationActions
+  @RelaxedMockK lateinit var mockItemViewModel: ItemViewModel
 
-    private var item1: Item = Item(
-        id = "id",
-        category = Category("id", "category1"),
-        name = "item1",
-        description = "description1",
-        author = "author1",
-        visibility = Visibility.PUBLIC,
-        quantity = 1,
-        location = mockk()
-    )
-    private var _uiState = MutableStateFlow(ItemUIState(item1))
-    private var mockUiState: StateFlow<ItemUIState> = _uiState
+  private var item1: Item =
+      Item(
+          id = "id",
+          category = Category("id", "category1"),
+          name = "item1",
+          description = "description1",
+          visibility = Visibility.PUBLIC,
+          quantity = 1,
+          location = mockk(),
+          idUser = "id_user")
+  private var _uiState = MutableStateFlow(ItemUIState(item1))
+  private var mockUiState: StateFlow<ItemUIState> = _uiState
 
-    @Before
-    fun testSetup() {
-        mockItemViewModel = mockk()
-        every { mockItemViewModel.uiState } returns mockUiState
+  @Before
+  fun testSetup() {
+    mockItemViewModel = mockk()
+    every { mockItemViewModel.uiState } returns mockUiState
 
-        mockNavActions = mockk<NavigationActions>()
-        every { mockNavActions.navigateTo(Route.HOME) } just Runs
-        every { mockNavActions.navigateTo(Route.LOGIN) } just Runs
+    mockNavActions = mockk<NavigationActions>()
+    every { mockNavActions.navigateTo(Route.HOME) } just Runs
+    every { mockNavActions.navigateTo(Route.LOGIN) } just Runs
 
-        composeTestRule.setContent {
-            InventoryViewItem(mockNavActions, mockItemViewModel)
-        }
-    }
+    composeTestRule.setContent { InventoryViewItem(mockNavActions, mockItemViewModel) }
+  }
 
-    @Test
-    fun testTest() {
-        assert(true)
-    }
+  @Test
+  fun testTest() {
+    assert(true)
+  }
 
-    @Test
-    fun topBarIsDisplayed() = run {
-        onComposeScreen<InventoryViewItem>(composeTestRule) {
-            topBar {
-                assertIsDisplayed()
-            }
-        }
-    }
+  @Test
+  fun topBarIsDisplayed() = run {
+    onComposeScreen<InventoryViewItem>(composeTestRule) { topBar { assertIsDisplayed() } }
+  }
 
-    @Test
-    fun bottomBarIsDisplayed() = run {
-        onComposeScreen<InventoryViewItem>(composeTestRule) {
-            bottomBar {
-                assertIsDisplayed()
-            }
-        }
-    }
+  @Test
+  fun bottomBarIsDisplayed() = run {
+    onComposeScreen<InventoryViewItem>(composeTestRule) { bottomBar { assertIsDisplayed() } }
+  }
 
-    @Test
-    fun itemNameUpdate() = run {
-        // Change the item name to trigger 'onValueChange' event to verify there is no erros
-        item1 = Item(
+  @Test
+  fun itemNameUpdate() = run {
+    // Change the item name to trigger 'onValueChange' event to verify there is no erros
+    item1 =
+        Item(
             id = "id",
             category = Category("id", "category1"),
             name = "item1_ALTNAME",
             description = "description1",
-            author = "author1",
             visibility = Visibility.PUBLIC,
             quantity = 1,
-            location = mockk()
-        )
-        onComposeScreen<InventoryViewItem>(composeTestRule) {
-            assertIsDisplayed()
-        }
-    }
+            location = mockk(),
+            idUser = "id_user")
+    onComposeScreen<InventoryViewItem>(composeTestRule) { assertIsDisplayed() }
+  }
 }
--- app/src/androidTest/java/com/android/partagix/screens/InventoryViewItem.kt
+++ app/src/androidTest/java/com/android/partagix/screens/InventoryViewItem.kt
@@ -9,8 +9,6 @@ class InventoryViewItem(semanticsProvider: SemanticsNodeInteractionsProvider) :
         semanticsProvider = semanticsProvider,
         viewBuilderAction = { hasTestTag("inventoryViewItem") }) {
 
-    val topBar: KNode = onNode { hasTestTag("inventoryViewItemTopBar") }
-    val bottomBar: KNode = onNode { hasTestTag("inventoryViewItemBottomBar") }
-
-
+  val topBar: KNode = onNode { hasTestTag("inventoryViewItemTopBar") }
+  val bottomBar: KNode = onNode { hasTestTag("inventoryViewItemBottomBar") }
 }
