2eeba452902d78ad5fc94bf6797eb4c3d1f4efb5
KassFlute <cassien.roth@epfl.ch>
2024-04-12 13:51:15 +0200

    Add testing screen for InventoryViewItem screen

---
+++ app/src/androidTest/java/com/android/partagix/screens/InventoryViewItem.kt
@@ -0,0 +1,16 @@
+package com.android.partagix.screens
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.github.kakaocup.compose.node.element.KNode
+
+class InventoryViewItem(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<InventoryViewItem>(
+        semanticsProvider = semanticsProvider,
+        viewBuilderAction = { hasTestTag("inventoryViewItem") }) {
+
+    val topBar: KNode = onNode { hasTestTag("inventoryViewItemTopBar") }
+    val bottomBar: KNode = onNode { hasTestTag("inventoryViewItemBottomBar") }
+
+
+}
