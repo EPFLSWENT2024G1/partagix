ff87939c5281927a0d84439544f3690318da26f5
Massimo Berardi <massimo.berardi@epfl.ch>
2024-04-16 11:48:15 +0200

    implementing a function retrieving the ordinal of a StampDimension object by its detailedDimension

--- app/src/main/java/com/android/partagix/model/StampViewModel.kt
+++ app/src/main/java/com/android/partagix/model/StampViewModel.kt
@@ -1,5 +1,22 @@
 package com.android.partagix.model
 
 import androidx.lifecycle.ViewModel
+import com.android.partagix.model.stampDimension.StampDimension
 
-class StampViewModel(itemId: String) : ViewModel() {}
+class StampViewModel(itemId: String) : ViewModel() {
+
+  /**
+   * Get the ordinal of the dimension, given the detailedDimension string.
+   *
+   * @param detailedDimension the detailed dimension string
+   * @return the ordinal of the dimension, or -1 if the dimension is not found
+   */
+  fun getDetailedDimensionOrdinal(detailedDimension: String): Int {
+    for (stampDimension in StampDimension.values()) {
+      if (stampDimension.detailedDimension == detailedDimension) {
+        return stampDimension.ordinal
+      }
+    }
+    return -1
+  }
+}
