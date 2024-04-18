529c18931aa50b930de00699d02ceb9b1a1b4cc4
KassFlute <cassien.roth@epfl.ch>
2024-04-18 15:39:11 +0200

    Adapt App.kt to new HomeScreen.kt parameters

--- app/src/main/java/com/android/partagix/ui/App.kt
+++ app/src/main/java/com/android/partagix/ui/App.kt
@@ -26,6 +26,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.android.partagix.model.HomeViewModel
 import com.android.partagix.model.InventoryViewModel
 import com.android.partagix.model.ItemViewModel
 import com.android.partagix.model.UserViewModel
@@ -148,7 +149,7 @@ class App(private val activity: MainActivity) : ComponentActivity(), SignInResul
     ) {
       composable(Route.BOOT) { BootScreen(authentication, navigationActions, modifier) }
       composable(Route.LOGIN) { LoginScreen(authentication, modifier) }
-      composable(Route.HOME) { HomeScreen(navigationActions) }
+      composable(Route.HOME) { HomeScreen(homeViewModel = HomeViewModel(), inventoryViewModel = InventoryViewModel(), navigationActions = navigationActions) }
       composable(Route.LOAN) {
         if (checkLocationPermissions()) {
           fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
@@ -163,7 +164,7 @@ class App(private val activity: MainActivity) : ComponentActivity(), SignInResul
               userViewModel = userViewModel,
               modifier = modifier)
         } else {
-          HomeScreen(navigationActions)
+            HomeScreen(homeViewModel = HomeViewModel(), inventoryViewModel = InventoryViewModel(), navigationActions = navigationActions)
         }
       }
 
