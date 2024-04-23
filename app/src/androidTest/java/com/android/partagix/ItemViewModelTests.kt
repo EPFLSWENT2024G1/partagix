809192b1a2f4d5de1c7dea4bf253a744ce0f0285
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-04-23 11:13:17 +0200

    all tests working

--- app/src/androidTest/java/com/android/partagix/ItemViewModelTests.kt
+++ app/src/androidTest/java/com/android/partagix/ItemViewModelTests.kt
@@ -9,8 +9,6 @@ import com.android.partagix.model.item.Item
 import com.android.partagix.model.visibility.Visibility
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.TaskCompletionSource
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
@@ -19,114 +17,157 @@ import com.google.firebase.firestore.util.Executors
 import com.google.firebase.firestore.util.Util
 import io.mockk.coVerify
 import io.mockk.every
-import io.mockk.just
 import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.runs
 import io.mockk.spyk
-import io.mockk.unmockkStatic
-import io.mockk.verify
-import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
-import java.sql.Date
 
 class ItemViewModelTests {
-    val emptyItem = Item("", Category("", ""), "", "", Visibility.PUBLIC, 1, Location(""))
-    private val _uiState = MutableStateFlow(ItemUIState(emptyItem))
+  val emptyItem = Item("", Category("", ""), "", "", Visibility.PUBLIC, 1, Location(""))
+
+  val itemWithID =
+      Item(
+          "8WuTkKJZLTAr6zs5L7rH",
+          Category("0", "Category 1"),
+          "test",
+          "test",
+          Visibility.PUBLIC,
+          1,
+          Location(""))
+
+  val itemWithIDmodified =
+      Item(
+          "8WuTkKJZLTAr6zs5L7rH",
+          Category("0", "Category 1"),
+          "modify",
+          "modified",
+          Visibility.FRIENDS,
+          3,
+          Location(""))
+  val itemNoID =
+      Item("", Category("0", "Category 1"), "test", "test", Visibility.PUBLIC, 1, Location(""))
+
+  @Test
+  fun testUpdateUiState() {
+    val _uiState = MutableStateFlow(ItemUIState(emptyItem))
+    val mockUiState: StateFlow<ItemUIState> = _uiState
+    val db = mockk<Database>()
+    val itemViewModel = spyk(ItemViewModel(db = db))
+    every { itemViewModel.uiState } returns mockUiState
+    every { itemViewModel.updateUiState(itemWithID) } answers
+        {
+          _uiState.value = ItemUIState(itemWithID)
+        }
+    itemViewModel.updateUiState(itemWithID)
+    assert(mockUiState.value.item == itemWithID)
+  }
+
+  @Test
+  fun testSaveNewItem() {
+    val _uiState = MutableStateFlow(ItemUIState(emptyItem))
     val mockUiState: StateFlow<ItemUIState> = _uiState
 
-    val itemWithID = Item("8WuTkKJZLTAr6zs5L7rH", Category("0", "Category 1"), "test", "test", Visibility.PUBLIC, 1, Location(""))
-    val itemNoID = Item("", Category("0", "Category 1"), "test", "test", Visibility.PUBLIC, 1, Location(""))
-    @Test
-    fun testUpdateUiState() {
-        val db = mockk<Database>()
-        val itemViewModel = spyk(ItemViewModel(db = db))
-        every { itemViewModel.uiState } returns mockUiState
-        every { itemViewModel.updateUiState(itemWithID) } answers {_uiState.value = ItemUIState(itemWithID)}
-        itemViewModel.updateUiState(itemWithID)
-        assert(mockUiState.value.item == itemWithID)
+    val taskCompletionSource = TaskCompletionSource<Void>()
 
+    val mockCollection = mockk<CollectionReference>()
+
+    val mockDocument = mockk<DocumentReference>()
+
+    every { mockCollection.document(any()) } returns mockDocument
+
+    every { mockCollection.document() } returns mockDocument
+
+    val documentId = "wkUYnOmKkNVWlo1K8/59SDD/JtCWCf9MvnAgSYx9BbCN8ZbuNU+uSqPWVDuFnVRB"
+    every { mockDocument.id } returns documentId
+
+    every { mockDocument.set(any()) } returns
+        taskCompletionSource.task.continueWith(
+            Executors.DIRECT_EXECUTOR, Util.voidErrorTransformer())
+
+    val mockDb: FirebaseFirestore = mockk {}
+
+    every { mockDb.collection(any()) } returns mockCollection
+
+    val mockQuerySnapshot = mockk<QuerySnapshot>()
+
+    val task = mockk<Task<QuerySnapshot>>()
+    every { task.result } returns mockQuerySnapshot
+
+    every { task.addOnSuccessListener(any()) } returns task
+    every { task.addOnFailureListener(any()) } returns task
+
+    every { mockCollection.get() } returns task
+
+    val db = spyk(Database(mockDb), recordPrivateCalls = true)
+
+    val itemViewModel = spyk(ItemViewModel(db = db))
+
+    runBlocking {
+      itemViewModel.save(itemNoID)
+
+      coVerify(exactly = 1) { itemViewModel.save(itemNoID) }
     }
+  }
 
-    @Test
-    fun testSaveNewItem() {
-        val db = mockk<Database>()
-        val itemViewModel = spyk(ItemViewModel(db = db))
+  @Test
+  fun testSaveAnItem() {
+    val _uiState = MutableStateFlow(ItemUIState(itemWithID))
+    val mockUiState: StateFlow<ItemUIState> = _uiState
 
-        every {db.getIdCategory(itemNoID.category.name){
-            db.createItem(
-                "userId",
-                Item(
-                    itemNoID.id,
-                    Category(it, itemNoID.category.name),
-                    itemNoID.name,
-                    itemNoID.description,
-                    itemNoID.visibility,
-                    itemNoID.quantity,
-                    itemNoID.location))}} answers {db.createItem("userId",itemNoID)}
+    val taskCompletionSource = TaskCompletionSource<Void>()
 
-        itemViewModel.save(itemNoID)
+    val mockCollection = mockk<CollectionReference>()
 
-        coVerify(exactly = 1) {db.createItem("userId",itemNoID)}
-    }
+    val mockDocument = mockk<DocumentReference>()
 
-    @Test
-    fun testSaveAnItem(){
-        val taskCompletionSource = TaskCompletionSource<Void>()
+    every { mockCollection.document(any()) } returns mockDocument
 
-        val mockCollection = mockk<CollectionReference>()
+    every { mockCollection.document() } returns mockDocument
 
-        val mockDocument = mockk<DocumentReference>()
+    val documentId = "wkUYnOmKkNVWlo1K8/59SDD/JtCWCf9MvnAgSYx9BbCN8ZbuNU+uSqPWVDuFnVRB"
+    every { mockDocument.id } returns documentId
 
-        every { mockCollection.document(any()) } returns mockDocument
+    every { mockDocument.set(any()) } returns
+        taskCompletionSource.task.continueWith(
+            Executors.DIRECT_EXECUTOR, Util.voidErrorTransformer())
 
-        every { mockCollection.document() } returns mockDocument
+    val mockDb: FirebaseFirestore = mockk {}
 
-        val documentId = "wkUYnOmKkNVWlo1K8/59SDD/JtCWCf9MvnAgSYx9BbCN8ZbuNU+uSqPWVDuFnVRB"
-        every { mockDocument.id } returns documentId
+    every { mockDb.collection(any()) } returns mockCollection
 
-        every { mockDocument.set(any()) } returns
-                taskCompletionSource.task.continueWith(Executors.DIRECT_EXECUTOR,
-                    Util.voidErrorTransformer()
-                )
+    val mockQuerySnapshot = mockk<QuerySnapshot>()
 
-        val mockDb: FirebaseFirestore = mockk {}
+    val task = mockk<Task<QuerySnapshot>>()
+    every { task.result } returns mockQuerySnapshot
 
-        every { mockDb.collection(any()) } returns mockCollection
+    every { task.addOnSuccessListener(any()) } returns task
+    every { task.addOnFailureListener(any()) } returns task
 
-        val mockQuerySnapshot = mockk<QuerySnapshot>()
+    every { mockCollection.get() } returns task
 
-        val task = mockk<Task<QuerySnapshot>>()
-        every { task.result } returns mockQuerySnapshot
+    val db = spyk(Database(mockDb), recordPrivateCalls = true)
 
-        every { task.addOnSuccessListener(any()) } returns task
-        every { task.addOnFailureListener(any()) } returns task
+    val itemViewModel = spyk(ItemViewModel(itemWithID, db = db))
 
-        every { mockCollection.get() } returns task
+    every { itemViewModel.uiState } returns mockUiState
 
-
-        val db = spyk(Database(mockDb), recordPrivateCalls = true)
-
-        val itemViewModel = spyk(ItemViewModel(db = db))
-
-        every { itemViewModel.uiState } returns mockUiState
-        every { itemViewModel.updateUiState(itemWithID) } answers {_uiState.value = ItemUIState(itemWithID)}
-        println("---- $_uiState.value.item")
-
-        runBlocking {
-            itemViewModel.save(itemWithID)
-
-            coVerify(exactly = 1) { itemViewModel.save(itemWithID) }
-            //assert(mockUiState.value.item == itemWithID)
+    every { itemViewModel.updateUiState(itemWithID) } answers
+        {
+          _uiState.value = ItemUIState(itemWithID)
         }
 
-        println("-------- $_uiState.value.item")
+    every { itemViewModel.updateUiState(itemWithIDmodified) } answers
+        {
+          _uiState.value = ItemUIState(itemWithIDmodified)
+        }
 
+    runBlocking {
+      itemViewModel.save(itemWithIDmodified)
 
+      coVerify(exactly = 1) { itemViewModel.save(itemWithIDmodified) }
+      assert(mockUiState.value.item == itemWithIDmodified)
     }
-
+  }
 }
-
