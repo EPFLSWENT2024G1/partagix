447f5cc5e48017e7b58fe71068122ec701f11e19
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-04-24 11:24:55 +0200

    be sure it's displayed before testing

--- app/src/androidTest/java/com/android/partagix/navigation/EndToEnd.kt
+++ app/src/androidTest/java/com/android/partagix/navigation/EndToEnd.kt
@@ -34,8 +34,8 @@ class EndToEnd {
 
     // Wait for the activity to be in the resumed state
     scenario.moveToState(Lifecycle.State.RESUMED)
-    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Home").isDisplayed() }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Home").isDisplayed() }
     // check that the bottom bar is well displayed
     ComposeScreen.onComposeScreen<NavigationBar>(composeTestRule) {
       homeButton { assertIsDisplayed() }
@@ -46,12 +46,14 @@ class EndToEnd {
       inventoryButton { performClick() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithTag("inventoryScreenFab").isDisplayed() }
     // click to create a new item
     ComposeScreen.onComposeScreen<InventoryScreen>(composeTestRule) {
       fab { assertIsDisplayed() }
       fab { performClick() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Create").isDisplayed() }
     // check that the create item screen is well displayed and that you can use the fields
     ComposeScreen.onComposeScreen<InventoryCreateOrEditScreen>(composeTestRule) {
       button { assertIsDisplayed() }
@@ -61,16 +63,20 @@ class EndToEnd {
       description { performTextInput("Christmas Tree") }
 
       category { performClick() }
+      composeTestRule.waitUntil { composeTestRule.onNodeWithTag("Category 1").isDisplayed() }
       composeTestRule.onNodeWithTag("Category 1").performClick()
       composeTestRule.onNodeWithText("Category 1").assertExists()
       category { performClick() }
+      composeTestRule.waitUntil { composeTestRule.onNodeWithTag("Category 3").isDisplayed() }
       composeTestRule.onNodeWithTag("Category 3").performClick()
       composeTestRule.onNodeWithText("Category 3").assertExists()
 
       visibility { performClick() }
+      composeTestRule.waitUntil { composeTestRule.onNodeWithTag("Friends").isDisplayed() }
       composeTestRule.onNodeWithTag("Friends").performClick()
       composeTestRule.onNodeWithText("Friends").assertExists()
       visibility { performClick() }
+      composeTestRule.waitUntil { composeTestRule.onNodeWithTag("Private").isDisplayed() }
       composeTestRule.onNodeWithTag("Private").performClick()
       composeTestRule.onNodeWithText("Private").assertExists()
 
@@ -78,40 +84,51 @@ class EndToEnd {
       navigationIcon { performClick() }
     }
 
+    composeTestRule.waitUntil {
+      composeTestRule.onNodeWithTag("inventoryScreenNoItemBox").isDisplayed()
+    }
     // check we are indeed in the inventory screen
     ComposeScreen.onComposeScreen<InventoryScreen>(composeTestRule) {
       noItemBox { assertIsDisplayed() }
       noItemText { assertIsDisplayed() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Account").isDisplayed() }
     // go on the account screen
     ComposeScreen.onComposeScreen<NavigationBar>(composeTestRule) {
       accountButton { performClick() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithTag("friendButton").isDisplayed() }
     // check we are indeed in the account screen and go to inventory via inventory button
     ComposeScreen.onComposeScreen<ViewAccount>(composeTestRule) {
       viewAccount { assertIsDisplayed() }
       inventoryButton { performClick() }
     }
 
+    composeTestRule.waitUntil {
+      composeTestRule.onNodeWithTag("inventoryScreenNoItemBox").isDisplayed()
+    }
     // check we are indeed in the inventory screen
     ComposeScreen.onComposeScreen<InventoryScreen>(composeTestRule) {
       noItemBox { assertIsDisplayed() }
       noItemText { assertIsDisplayed() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Account").isDisplayed() }
     // go to the account screen
     ComposeScreen.onComposeScreen<NavigationBar>(composeTestRule) {
       accountButton { performClick() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithTag("viewAccount").isDisplayed() }
     // check we are indeed in the account screen and go to inventory via go back button
     ComposeScreen.onComposeScreen<ViewAccount>(composeTestRule) {
       viewAccount { assertIsDisplayed() }
       backButton { performClick() }
     }
 
+    composeTestRule.waitUntil { composeTestRule.onNodeWithText("Home").isDisplayed() }
     // go to the home screen
     ComposeScreen.onComposeScreen<NavigationBar>(composeTestRule) {
       // faudrait accepter de base la loca map sinon ça fait crash
