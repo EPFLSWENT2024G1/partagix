4167ff5ea1d6a6da5ea83440554b143667cc931d
Mass-14 <126057141+Mass-14@users.noreply.github.com>
2024-04-29 15:37:16 +0200

    tested without the previous testTag now C.kt can be safely delete

--- app/src/main/java/com/android/partagix/MainActivity.kt
+++ app/src/main/java/com/android/partagix/MainActivity.kt
@@ -29,8 +29,7 @@ class MainActivity : ComponentActivity() {
       PartagixAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier =
-                Modifier.fillMaxSize().semantics { /*testTag = C.Tag.main_screen_container*/},
+            modifier = Modifier.fillMaxSize().semantics {},
             color = MaterialTheme.colorScheme.background) {
               app.Create()
             }
