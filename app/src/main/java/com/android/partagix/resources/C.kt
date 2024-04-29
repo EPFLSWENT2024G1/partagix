191085c8ab6416949979a75457da1dbe77f3e24b
Mass-14 <126057141+Mass-14@users.noreply.github.com>
2024-04-29 10:10:08 +0200

    Safely deleted unused files (and tests) from the project creation.

--- app/src/main/java/com/android/partagix/resources/C.kt
+++ app/src/main/java/com/android/partagix/resources/C.kt
@@ -4,9 +4,7 @@ package com.android.partagix.resources
 object C {
   object Tag {
     const val greeting = "main_screen_greeting"
-    const val greeting_robo = "second_screen_greeting"
 
     const val main_screen_container = "main_screen_container"
-    const val second_screen_container = "second_screen_container"
   }
 }
