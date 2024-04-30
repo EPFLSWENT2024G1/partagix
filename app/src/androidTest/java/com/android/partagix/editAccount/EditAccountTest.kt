16ba9159768a2a3bea5ad95e2608f8dc1e96835a
KassFlute <cassien.roth@epfl.ch>
2024-04-30 20:52:00 +0200

    Add tests for the EditAccount screen

---
+++ app/src/androidTest/java/com/android/partagix/editAccount/EditAccountTest.kt
@@ -0,0 +1,151 @@
+package com.android.partagix.editAccount
+
+import android.location.Location
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.partagix.model.UserUIState
+import com.android.partagix.model.UserViewModel
+import com.android.partagix.model.category.Category
+import com.android.partagix.model.inventory.Inventory
+import com.android.partagix.model.item.Item
+import com.android.partagix.model.user.User
+import com.android.partagix.screens.EditAccount
+import com.android.partagix.ui.navigation.NavigationActions
+import com.android.partagix.ui.navigation.Route
+import com.android.partagix.ui.screens.EditAccount
+import com.google.common.base.Verify.verify
+import com.kaspersky.components.composesupport.config.withComposeSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import io.github.kakaocup.compose.node.element.ComposeScreen
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class EditAccountTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @RelaxedMockK lateinit var mockNavActions: NavigationActions
+
+  @RelaxedMockK lateinit var mockUserViewModel: UserViewModel
+
+  private lateinit var emptyMockUiState: MutableStateFlow<UserUIState>
+  private lateinit var nonEmptyMockUiState: MutableStateFlow<UserUIState>
+
+  val cat1 = Category("1", "Category 1")
+  val vis1 = com.android.partagix.model.visibility.Visibility.PUBLIC
+  val loc1 = Location("1")
+
+  private val userOne: User =
+      User(
+          "id1",
+          "name1",
+          "address1",
+          "rank1",
+          Inventory("id1", listOf(Item("1", cat1, "Name 1", "Description 1", vis1, 1, loc1))))
+
+  @Before
+  fun testSetup() {
+    nonEmptyMockUiState = MutableStateFlow(UserUIState(userOne))
+
+    mockUserViewModel = mockk()
+    every { mockUserViewModel.updateUser(any()) } just Runs
+
+    mockNavActions = mockk<NavigationActions>()
+    every { mockNavActions.navigateTo(Route.HOME) } just Runs
+    every { mockNavActions.navigateTo(Route.LOGIN) } just Runs
+    every { mockNavActions.navigateTo(Route.ACCOUNT) } just Runs
+    every { mockNavActions.navigateTo(Route.INVENTORY) } just Runs
+    every { mockNavActions.navigateTo(Route.LOAN) } just Runs
+    every { mockNavActions.navigateTo(Route.BOOT) } just Runs
+    every { mockNavActions.navigateTo(Route.VIEW_ITEM) } just Runs
+    every { mockNavActions.goBack() } just Runs
+  }
+
+  @Test
+  fun isDisplayedGoodUser() {
+    every { mockUserViewModel.uiState } returns nonEmptyMockUiState
+    every { mockUserViewModel.getLoggedUserId() } returns "id1"
+    composeTestRule.setContent {
+      EditAccount(
+          modifier = Modifier,
+          userViewModel = mockUserViewModel,
+          navigationActions = mockNavActions)
+    }
+
+    ComposeScreen.onComposeScreen<EditAccount>(composeTestRule) {
+      topBar { assertIsDisplayed() }
+      backButton { assertIsDisplayed() }
+      bottomNavBar { assertIsDisplayed() }
+      mainContent { assertIsDisplayed() }
+      userImage { assertIsDisplayed() }
+      username { assertIsDisplayed() }
+      usernameField { assertIsDisplayed() }
+      addressField { assertIsDisplayed() }
+      actionButtons { assertIsDisplayed() }
+      saveButton { assertIsDisplayed() }
+    }
+  }
+
+  @Test
+  fun isDisplayedBadUser() {
+    every { mockUserViewModel.uiState } returns nonEmptyMockUiState
+    every { mockUserViewModel.getLoggedUserId() } returns "not_id1"
+    composeTestRule.setContent {
+      EditAccount(
+          modifier = Modifier,
+          userViewModel = mockUserViewModel,
+          navigationActions = mockNavActions)
+    }
+
+    ComposeScreen.onComposeScreen<EditAccount>(composeTestRule) {
+      topBar { assertIsDisplayed() }
+      backButton { assertIsDisplayed() }
+      bottomNavBar { assertIsDisplayed() }
+      notYourAccount { assertIsDisplayed() }
+    }
+  }
+
+  @Test
+  fun backButtonClick() {
+    every { mockUserViewModel.uiState } returns nonEmptyMockUiState
+    every { mockUserViewModel.getLoggedUserId() } returns "id1"
+    composeTestRule.setContent {
+      EditAccount(
+          modifier = Modifier,
+          userViewModel = mockUserViewModel,
+          navigationActions = mockNavActions)
+    }
+
+    ComposeScreen.onComposeScreen<EditAccount>(composeTestRule) { backButton { performClick() } }
+
+    verify(exactly = 1) { mockNavActions.goBack() }
+  }
+
+  @Test
+  fun saveButtonClick() {
+    every { mockUserViewModel.uiState } returns nonEmptyMockUiState
+    every { mockUserViewModel.getLoggedUserId() } returns "id1"
+    composeTestRule.setContent {
+      EditAccount(
+          modifier = Modifier,
+          userViewModel = mockUserViewModel,
+          navigationActions = mockNavActions)
+    }
+
+    ComposeScreen.onComposeScreen<EditAccount>(composeTestRule) { saveButton { performClick() } }
+
+    verify(exactly = 1) { mockUserViewModel.updateUser(any()) }
+    verify(exactly = 1) { mockNavActions.goBack() }
+  }
+}
