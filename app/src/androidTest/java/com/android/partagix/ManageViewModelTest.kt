10132c968e2976d030da0371098ae892aefc5094
Patrick Pataky <patrick.pataky@epfl.ch>
2024-05-02 23:18:48 +0200

    Added the little coverage it needs to pass the CI. Also refactored a little bit getLoanRequests

--- app/src/androidTest/java/com/android/partagix/ManageViewModelTest.kt
+++ app/src/androidTest/java/com/android/partagix/ManageViewModelTest.kt
@@ -3,6 +3,7 @@ package com.android.partagix
 import android.location.Location
 import com.android.partagix.model.Database
 import com.android.partagix.model.ManageLoanViewModel
+import com.android.partagix.model.auth.Authentication
 import com.android.partagix.model.category.Category
 import com.android.partagix.model.inventory.Inventory
 import com.android.partagix.model.item.Item
@@ -10,13 +11,13 @@ import com.android.partagix.model.loan.Loan
 import com.android.partagix.model.loan.LoanState
 import com.android.partagix.model.user.User
 import com.android.partagix.model.visibility.Visibility
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import io.mockk.Runs
 import io.mockk.clearAllMocks
 import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.mockkObject
 import io.mockk.spyk
 import java.util.Date
 import java.util.concurrent.CountDownLatch
@@ -26,9 +27,9 @@ import org.junit.Before
 import org.junit.Test
 
 class ManageViewModelTest {
-  val db = mockk<Database>()
-  @RelaxedMockK lateinit var fire: FirebaseAuth
-  val item1 =
+  private val db = mockk<Database>()
+
+  private val item1 =
       Item(
           "item1",
           Category("0", "Category 1"),
@@ -39,9 +40,10 @@ class ManageViewModelTest {
           Location(""),
           idUser = "8WuTkKJZLTAr6zs5L7rH")
 
-  val user =
+  private val user =
       User("8WuTkKJZLTAr6zs5L7rH", "user1", "", "", Inventory("8WuTkKJZLTAr6zs5L7rH", emptyList()))
-  val loan1 =
+
+  private val loan1 =
       Loan(
           id = "1",
           commentLoaner = "commentLoaner",
@@ -54,7 +56,7 @@ class ManageViewModelTest {
           reviewOwner = "reviewOwner",
           startDate = Date(),
           state = LoanState.PENDING)
-  val loan2 =
+  private val loan2 =
       Loan(
           id = "2",
           commentLoaner = "commentLoaner",
@@ -67,7 +69,8 @@ class ManageViewModelTest {
           reviewOwner = "reviewOwner",
           startDate = Date(),
           state = LoanState.PENDING)
-  val loan3 =
+
+  private val loan3 =
       Loan(
           id = "3",
           commentLoaner = "commentLoaner",
@@ -84,8 +87,6 @@ class ManageViewModelTest {
   @Before
   fun setup() {
     clearAllMocks()
-    fire = mockk()
-    every { fire.currentUser } returns mockk { every { uid } returns "8WuTkKJZLTAr6zs5L7rH" }
     every { db.getItems(any()) } answers
         {
           firstArg<(List<Item>) -> Unit>().invoke(listOf(item1, item1, item1))
@@ -105,20 +106,29 @@ class ManageViewModelTest {
 
   @Test
   fun testAcceptAndDecline() {
+    val mockUser = mockk<FirebaseUser>()
+    mockkObject(Authentication)
+    every { Authentication.getUser() } returns mockUser
+    every { mockUser.uid } returns "8WuTkKJZLTAr6zs5L7rH"
+
     val latch = CountDownLatch(1)
     val manageViewModel = spyk(ManageLoanViewModel(db = db, latch = latch))
     latch.await()
+
     manageViewModel.update(
         listOf(item1, item1, item1),
         listOf(user, user, user),
         listOf(loan1, loan2, loan3),
         listOf(false, false, false))
     manageViewModel.acceptLoan(loan1, 0)
+
     assert(manageViewModel.uiState.value.items == listOf(item1, item1))
     assert(manageViewModel.uiState.value.loans == listOf(loan2, loan3))
     assert(manageViewModel.uiState.value.users == listOf(user, user))
     assert(manageViewModel.uiState.value.expanded == listOf(false, false))
+
     manageViewModel.declineLoan(loan3, 1)
+
     assert(manageViewModel.uiState.value.items == listOf(item1))
     assert(manageViewModel.uiState.value.loans == listOf(loan2))
     assert(manageViewModel.uiState.value.users == listOf(user))
@@ -126,26 +136,34 @@ class ManageViewModelTest {
   }
 
   @Test
-  fun testGetLoanRequests() {
+  fun testGetLoanRequestsNoUser() {
+    mockkObject(Authentication)
+    every { Authentication.getUser() } returns null
+
     val latch = CountDownLatch(1)
     val manageViewModel = spyk(ManageLoanViewModel(db = db, latch = latch))
     latch.await()
+
     runBlocking {
-      println(manageViewModel.uiState.value.users)
-      assert(manageViewModel.uiState.value.items == listOf(item1, item1, item1))
-      assert(manageViewModel.uiState.value.users == listOf(user, user, user))
-      assert(manageViewModel.uiState.value.loans == listOf(loan1, loan2, loan3))
-      assert(manageViewModel.uiState.value.expanded == listOf(false, false, false))
+      assert(manageViewModel.uiState.value.users.isEmpty())
+      assert(manageViewModel.uiState.value.items.isEmpty())
+      assert(manageViewModel.uiState.value.loans.isEmpty())
+      assert(manageViewModel.uiState.value.expanded.isEmpty())
     }
   }
 
   @Test
-  fun testGetLoanRequestsNotNull() {
+  fun testGetLoanRequestsUserNotNull() {
+    val mockUser = mockk<FirebaseUser>()
+    mockkObject(Authentication)
+    every { Authentication.getUser() } returns mockUser
+    every { mockUser.uid } returns "8WuTkKJZLTAr6zs5L7rH"
+
     val latch = CountDownLatch(1)
-    val manageViewModel = spyk(ManageLoanViewModel(db = db, firebaseAuth = fire, latch = latch))
+    val manageViewModel = spyk(ManageLoanViewModel(db = db, latch = latch))
     latch.await()
+
     runBlocking {
-      println(manageViewModel.uiState.value.users)
       assert(manageViewModel.uiState.value.items == listOf(item1, item1, item1))
       assert(manageViewModel.uiState.value.users == listOf(user, user, user))
       assert(manageViewModel.uiState.value.loans == listOf(loan1, loan2, loan3))
