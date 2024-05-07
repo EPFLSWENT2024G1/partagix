385e7caf469107f5a4dd02fa8cee734a5e35c43a
KassFlute <cassien.roth@epfl.ch>
2024-05-07 18:07:58 +0200

    Update BorrowViewModel.kt

--- app/src/main/java/com/android/partagix/model/BorrowViewModel.kt
+++ app/src/main/java/com/android/partagix/model/BorrowViewModel.kt
@@ -1,35 +1,37 @@
 package com.android.partagix.model
 
 import androidx.lifecycle.ViewModel
+import com.android.partagix.model.item.Item
 import com.android.partagix.model.loan.Loan
 import com.android.partagix.model.loan.LoanState
 import java.sql.Date
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-val emptyLoan =
-    Loan(
-        "id",
-        "id_owner",
-        "id_loaner",
-        "id_item",
-        Date(0),
-        Date(0),
-        "r",
-        "r",
-        "c",
-        "c",
-        LoanState.PENDING)
+val emptyLoan = Loan("", "", "", "", Date(0), Date(0), "", "", "", "", LoanState.PENDING)
 
-class BorrowViewModel(db: Database = Database(), loan: Loan = emptyLoan) : ViewModel() {
+class BorrowViewModel(db: Database = Database(), item: Item) : ViewModel() {
 
   private val database = db
 
-  private val _uiState = MutableStateFlow(loan)
+  private val _uiState = MutableStateFlow(emptyLoan)
   val uiState: StateFlow<Loan> = _uiState
 
+  private val _item = item
+
   init {
+    // Set the loaner id to the current logged user
     _uiState.value = emptyLoan
+    database.getCurrentUser { user -> _uiState.value = _uiState.value.copy(idLoaner = user.id) }
+
+    // Set the loan request to pending
+    _uiState.value = _uiState.value.copy(state = LoanState.PENDING)
+
+    // Set the item id in the loan
+    _uiState.value = _uiState.value.copy(idItem = item.id)
+
+    // Set the owner id in the loan to the owner of the item
+    _uiState.value = _uiState.value.copy(idOwner = _item.idUser)
   }
 
   /**
