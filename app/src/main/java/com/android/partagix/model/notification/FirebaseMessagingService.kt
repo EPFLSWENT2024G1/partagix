fa72bf379eb95c426cf2ae58e132a1a4ff654d8d
Patrick Pataky <patrick.pataky@epfl.ch>
2024-05-09 09:06:57 +0200

    Started adding notification requests

--- app/src/main/java/com/android/partagix/model/notification/FirebaseMessagingService.kt
+++ app/src/main/java/com/android/partagix/model/notification/FirebaseMessagingService.kt
@@ -1,8 +1,14 @@
 package com.android.partagix.model.notification
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.android.partagix.MainActivity
 import com.android.partagix.R
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
@@ -11,7 +17,37 @@ import com.google.firebase.messaging.RemoteMessage
 
 
 class FirebaseMessagingService : FirebaseMessagingService() {
-  private var context: Context? = null
+  // To be initialized using setContext()
+  private var context: MainActivity? = null
+
+  private val requestPermissionLauncher = context.registerForActivityResult(
+    ActivityResultContracts.RequestPermission(),
+  ) { isGranted: Boolean ->
+    if (isGranted) {
+      // FCM SDK (and your app) can post notifications.
+    } else {
+      // TODO: Inform user that that your app will not show notifications.
+    }
+  }
+
+  private fun askNotificationPermission() {
+    // This is only necessary for API level >= 33 (TIRAMISU)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+        PackageManager.PERMISSION_GRANTED
+      ) {
+        // FCM SDK (and your app) can post notifications.
+      } else if (context?.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) == true) {
+        // TODO: display an educational UI explaining to the user the features that will be enabled
+        //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+        //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+        //       If the user selects "No thanks," allow the user to continue without notifications.
+      } else {
+        // Directly ask for the permission
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+      }
+    }
+  }
 
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
     // TODO(developer): Handle FCM messages here.
@@ -46,11 +82,16 @@ class FirebaseMessagingService : FirebaseMessagingService() {
     sendRegistrationToServer(token)
   }
 
-  fun setContext(context: Context) {
+  fun setContext(context: MainActivity) {
     this.context = context
   }
 
   fun getToken() {
+    if (context == null) {
+      Log.e(TAG, "Context is not set, cannot get token")
+      return
+    }
+
     FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
       if (!task.isSuccessful) {
         Log.w(TAG, "Fetching FCM registration token failed", task.exception)
@@ -62,7 +103,6 @@ class FirebaseMessagingService : FirebaseMessagingService() {
 
       // Log and toast
       Log.d(TAG, token)
-      //val msg = getString(R.string.msg_token_fmt, token)
       val msg = "Token: $token"
       Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     })
