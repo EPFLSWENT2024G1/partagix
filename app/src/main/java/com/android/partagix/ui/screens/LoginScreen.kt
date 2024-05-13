42be810e86940ef97c8265dbf8e1957a067bce71
KassFlute <cassien.roth@epfl.ch>
2024-05-13 10:49:03 +0200

    Redesign the login screen

--- app/src/main/java/com/android/partagix/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/android/partagix/ui/screens/LoginScreen.kt
@@ -3,19 +3,29 @@ package com.android.partagix.ui.screens
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -27,16 +37,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import com.android.partagix.R
 import com.android.partagix.model.auth.Authentication
 
@@ -54,87 +67,152 @@ fun LoginScreen(authentication: Authentication, modifier: Modifier = Modifier) {
   val sheetState = rememberModalBottomSheetState()
   var showSignIn by remember { mutableStateOf(false) }
 
-  Column(
-      modifier = modifier.padding(15.dp).testTag("LoginScreen"),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.spacedBy(50.dp, Alignment.CenterVertically),
-  ) {
-    Text(
-        text = "Your neighbors are already here !",
-        modifier = modifier.testTag("LoginTitle"),
-        style =
-            TextStyle(
-                fontSize = 28.sp,
-                fontFamily = FontFamily(Font(R.font.roboto)),
-                fontWeight = FontWeight(500),
-                color = Color(0xFF000000),
-            ))
+  Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+    Box(
+        modifier =
+            Modifier.fillMaxWidth()
+                .height(150.dp)
+                .background(
+                    MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp))) {
+          Text(
+              text = "Partagix",
+              style = MaterialTheme.typography.displayLarge,
+              color = MaterialTheme.colorScheme.onPrimary,
+              modifier = Modifier.align(Alignment.Center))
+        }
 
-    OutlinedButton(
-        modifier = modifier.testTag("PopUpLoginButton").fillMaxWidth().padding(16.dp),
-        onClick = { showSignIn = true },
-        shape = RoundedCornerShape(10.dp),
-        border = BorderStroke(1.dp, Color(0xffafafaf)),
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
       Text(
-          text = "Start borrowing now",
-          style =
-              TextStyle(
-                  fontSize = 28.sp,
-                  fontFamily = FontFamily(Font(R.font.roboto)),
-                  fontWeight = FontWeight(500),
-                  color = Color(0xFF000000),
-                  textAlign = TextAlign.Center,
-              ),
-          modifier = modifier.padding(12.dp).testTag("LoginButtonOpenBottomSheet"))
+          text = "Your neighbors are already here !",
+          style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.SemiBold),
+      )
+      val images =
+          listOf(
+              R.drawable.multiprises,
+              R.drawable.holzkohle,
+              R.drawable.ks28,
+              R.drawable.remorque,
+          )
+      Box(
+        modifier = Modifier.fillMaxWidth().graphicsLayer {  }
+      ) {
+        ImageGrid(modifier = modifier
+          .align(Alignment.Center),
+          images = images)
+      }
     }
+
+    Box(
+        modifier = Modifier.fillMaxWidth().padding(16.dp).align(Alignment.BottomCenter),
+        contentAlignment = Alignment.Center) {
+          Button(
+              onClick = { showSignIn = true },
+              modifier = Modifier.fillMaxWidth().height(60.dp),
+          ) {
+            Text(text = "Start Borrowing now", style = MaterialTheme.typography.titleLarge)
+          }
+        }
   }
+
   if (showSignIn) {
     ModalBottomSheet(
         onDismissRequest = { showSignIn = false },
         sheetState = sheetState,
-        modifier = modifier.fillMaxHeight(.35f),
+        modifier = modifier.fillMaxHeight(.4f),
     ) {
       Column(
           verticalArrangement = Arrangement.Top,
           horizontalAlignment = Alignment.CenterHorizontally,
           modifier = modifier.fillMaxSize().testTag("LoginScreen2"),
       ) {
-        OutlinedButton(
+        Text(
+            text = "Sign in to start Borrowing !",
+            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+        )
+        LoginOptionButton(
+            modifier = modifier,
+            name = "Sign in with Google",
+            icon = R.drawable.google,
             onClick = {
               Log.w(TAG, "push button to sign in")
-
               authentication.signIn()
-            },
-            shape = RoundedCornerShape(20.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-            border = BorderStroke(1.dp, Color(0xFFDADCE0)),
-            modifier =
-                modifier
-                    .fillMaxWidth()
-                    .padding(PaddingValues(start = 24.dp, end = 24.dp, top = 32.dp, bottom = 32.dp))
-                    .testTag("LoginButton"),
-        ) {
-          Row(
-              verticalAlignment = Alignment.CenterVertically,
-              modifier = modifier.padding(horizontal = 24.dp)) {
-                Image(
-                    modifier = modifier.size(24.dp),
-                    painter = painterResource(id = R.drawable.google),
-                    contentDescription = "Google Icon",
-                )
-                Text(
-                    text = "Sign in with Google",
-                    color = Color.Black,
-                    textAlign = TextAlign.Center,
-                    style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Medium),
-                    modifier =
-                        modifier
-                            .wrapContentHeight(align = Alignment.CenterVertically)
-                            .padding(6.dp))
-              }
-        }
+            })
       }
     }
   }
+}
+
+@Composable
+fun ImageGrid(modifier: Modifier = Modifier, images: List<Int>) {
+  LazyVerticalGrid(
+      columns = GridCells.Fixed(2),
+      contentPadding = PaddingValues(20.dp),
+      verticalArrangement = Arrangement.spacedBy(10.dp),
+      horizontalArrangement = Arrangement.spacedBy(10.dp),
+      userScrollEnabled = true,
+      modifier = Modifier.fillMaxWidth()) {
+        items(images.size) { index ->
+          Box(
+              modifier = Modifier
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(8.dp))
+                .border(2.dp, MaterialTheme.colorScheme.primary),
+
+              contentAlignment = Alignment.Center
+          ) {
+            Image(
+              painter = painterResource(id = images[index]),
+              contentDescription = null,
+              contentScale = ContentScale.Crop,
+              modifier = Modifier.aspectRatio(1f).clip(RoundedCornerShape(8.dp))
+            )
+          }
+        }
+      }
+}
+
+@Composable
+fun LoginOptionButton(modifier: Modifier = Modifier, name: String, icon: Int, onClick: () -> Unit) {
+  OutlinedButton(
+      onClick = onClick,
+      shape = RoundedCornerShape(20.dp),
+      colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+      border = BorderStroke(1.dp, Color(0xFFDADCE0)),
+      modifier =
+          Modifier.fillMaxWidth()
+              .padding(PaddingValues(start = 24.dp, end = 24.dp, top = 32.dp, bottom = 32.dp))
+              .testTag("LoginButton"),
+  ) {
+    Row(verticalAlignment = Alignment.Top, modifier = modifier.padding(vertical = 15.dp)) {
+      Image(
+          modifier = modifier.size(24.dp),
+          alignment = Alignment.TopStart,
+          painter = painterResource(id = icon),
+          contentDescription = "Google Icon",
+      )
+      Text(
+          text = name,
+          color = Color.Black,
+          textAlign = TextAlign.Center,
+          style = TextStyle(fontSize = 15.sp, fontWeight = FontWeight.Medium),
+          modifier = modifier.wrapContentHeight(align = Alignment.CenterVertically))
+    }
+  }
+}
+
+@Preview
+@Composable
+fun GridPreview() {
+  ImageGrid(
+      images =
+          listOf(
+              R.drawable.ic_launcher_background,
+              R.drawable.ic_launcher_background,
+              R.drawable.ic_launcher_background,
+              R.drawable.ic_launcher_background))
 }
