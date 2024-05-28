6e0f91af22f6a98a7f6ac7bc4e4fc5b447a326c2
LozanoCyrielle <cyrielle.lozano@epfl.ch>
2024-05-28 11:31:57 +0200

    Update LocationPicker.kt

--- app/src/main/java/com/android/partagix/ui/components/locationPicker/LocationPicker.kt
+++ app/src/main/java/com/android/partagix/ui/components/locationPicker/LocationPicker.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
